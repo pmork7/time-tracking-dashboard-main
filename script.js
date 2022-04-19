e366c086c61bb3133cb6611b6880fbfb85237ee5
Paul Mork <paul.a.mork@gmail.com>
2022-04-18 22:44:24 -0700

    Finished website layout. Starting to add JS

---
+++ script.js
@@ -0,0 +1,5 @@
+let currentlySelected = "weekly"
+
+let dailyItems = document.getElementsByClassName("daily");
+let weeklyItems = document.getElementsByClassName("weekly");
+let monthlyItems = document.getElementsByClassName("monthly");
